845741676e6e9bf5e9024f9c3cec24034b309abf
SvetAlexa <dazzle@bk.ru>
2023-10-24 00:59:38 +0300

    создать схемы и модели ресурсов

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb', {
+  useNewUrlParser: true,
+})
+  .then(() => console.log('connected to DB'))
+  .catch(() => console.log('no connection'));
 
 const app = express();
 
---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Введите e-mail в правильном формате',
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('user', userSchema);
